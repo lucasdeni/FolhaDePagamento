5a46d1dd291d8b4974e353af56cae95ddc1acced
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-11-28 16:25:26 -0300

    Adicionar arquivos de projeto.

---
+++ FolhaDePagamento/Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FolhaDePagamento
+{
+    class FuncionarioTercerizado : Funcionário
+    {
+        public double Adicional { get; set; } //Declaração das variaveis e encapsulamento
+        public FuncionarioTercerizado()
+        {
+            //Construtor padrão
+        }
+        public FuncionarioTercerizado(string nome, int horas, double valor, double adicional) : base(nome, horas, valor) //Construtor com parametros
+        {
+            Adicional = adicional;
+        }
+        public override double Pagamento() //Calcular o pagamento do funcionário tercerizado
+        {
+            return Horas * Valor + Adicional * 1.1;
+        }
+    }
+}
